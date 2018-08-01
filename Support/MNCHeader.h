52a441451ea41327c34e688b8a84ac85e53715c4
thomasmeng <thomasmeng@futunn.com>
2018-08-01 23:47:20 +0800

    [add fromBrach]

---
+++ Support/MNCHeader.h
@@ -0,0 +1,49 @@
+//
+//  MNCHeader.h
+//  WeatherForecastDemo
+//
+//  Created by thomasmeng on 2018/8/1.
+//  Copyright © 2018年 thomasmeng. All rights reserved.
+//
+
+#ifndef MNCHeader_h
+#define MNCHeader_h
+
+//宏定义
+#define kScreenW [UIScreen mainScreen].bounds.size.width
+#define kScreenH [UIScreen mainScreen].bounds.size.height
+/**
+#pragma mark - Lift cycle
+#pragma mark - Overridden methods
+#pragma mark - Public methods
+#pragma mark - Private methods
+#pragma mark - Notification
+#pragma mark - KVO
+#pragma mark - IBAction/ClickActions
+#pragma mark - protocol
+#pragma mark - UItextFieldDelegate
+#pragma mark - UItableViewDataSource
+#pragma mark - UItableViewDelegate
+#pragma mark - setter && getter
+ */
+
+
+
+//常量定义
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"admin_area";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"cond_txt_d";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"cond_txt_n";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"date";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"wind_sc";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"cityLat";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"location";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"tmp_max";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"tmp_min";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"pcpn";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"vis";
+static NSString * const kFTQFilterGridViewColumnsKeyAnchor = @"hum";
+
+static NSString * const FTImanfHeight = 50.0;
+
+
+#endif /* MNCHeader_h */
